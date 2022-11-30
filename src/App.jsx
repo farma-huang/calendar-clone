3941a7905f724fbeaad6841a35059516ad10acf6
Farma Huang <farmahuang@gmail.com>
2022-11-30 10:54:50 +0800

    add simple font awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Calender from './components/Calender';
+import Header from './components/Header';
 import './App.css'
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
 
   return (
     <div className="App w-screen h-screen flex flex-col">
-      <div className="header border-b-2 border-[#dadce0] h-16 w-screen"></div>
+      {/* <div className="header border-b-2 border-[#dadce0] h-16 w-screen">
+          
+      </div> */}
+      <Header />
       <div className="content-wrapper w-screen h-full flex">
         <div className="slider border-r-2 border-[#dadce0] h-full w-64"></div>
         <div className="content grow h-full">
